adbad157e689ff02f5b00dc5ea51907f503c6b0c
halityurtsever <halityurtsever@gmail.com>
2017-02-17 15:15:27 +0300

    Selection sort algoritm is added.

---
+++ Algorithms/Sorting/SelectionSort/SelectionSort.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeHelpers;
+
+namespace Algorithms.Sorting.SelectionSort
+{
+    /** Selection sort algorithm
+     * Swaps [i] and [i + 1] if [i] smaller then [i + 1] until m_Array ordered
+     * 
+     * Initial m_Array    -> 7 - 3 - 9 - 5 - 2 - 8 - 6 - 1 - 4
+     * 
+     * Pass 1             -> 3 - 7 - 9 - 5 - 2 - 8 - 6 - 1 - 4
+     *                       ^   ^
+     *                       swap 7 and 3
+     * 
+     * Pass 2             -> 3 - 7 - 5 - 9 - 2 - 8 - 6 - 1 - 4
+     *                               ^   ^
+     *                               swap 9 and 5
+     * 
+     * Pass 3             -> 3 - 7 - 5 - 2 - 9 - 8 - 6 - 1 - 4
+     *                                   ^   ^
+     *                                   swap 9 and 2
+     * and goes on...
+     */
+
+    public class SelectionSort
+    {
+        //################################################################################
+        #region Fields
+
+        private readonly int[] m_Array;
+
+        #endregion
+
+        //################################################################################
+        #region Constructor
+
+        public SelectionSort(int[] array)
+        {
+            m_Array = array;
+        }
+
+        #endregion
+
+        //################################################################################
+        #region Public Implementation
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Sort()
+        {
+            int minIndex = 0;
+
+            for (int i = 0; i < m_Array.Length; i++)
+            {
+                for (int j = i; j < m_Array.Length; j++)
+                {
+                    if (m_Array[j] < m_Array[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if (i == minIndex)
+                {
+                    minIndex++;
+                    continue;
+                }
+
+                var temp = m_Array[i];
+                m_Array[i] = m_Array[minIndex];
+                m_Array[minIndex] = temp;
+            }
+        }
+
+        public void TraceSort(Tracer tracer)
+        {
+            int minIndex = 0;
+#if TRACE
+            tracer.CurrentArray(m_Array);
+            var sortCount = 0;
+#endif
+            for (int i = 0; i < m_Array.Length; i++)
+            {
+                for (int j = i; j < m_Array.Length; j++)
+                {
+                    if (m_Array[j] < m_Array[minIndex])
+                    {
+                        minIndex = j;
+#if TRACE
+                        sortCount++;
+#endif
+                    }
+                }
+
+                if (i == minIndex)
+                {
+                    minIndex++;
+                    continue;
+                }
+
+                var temp = m_Array[i];
+                m_Array[i] = m_Array[minIndex];
+                m_Array[minIndex] = temp;
+#if TRACE
+                tracer.CurrentArray(m_Array);
+#endif
+            }
+#if TRACE
+            tracer.WriteOutput($"TOTAL SORT COUNT: {sortCount}");
+#endif
+        }
+
+        #endregion
+    }
+}
---
+++ Algorithms/Sorting/SelectionSort_Tests/SelectionSort_Tests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelpers;
+
+namespace Algorithms.Sorting.SelectionSort.Tests
+{
+    [TestClass()]
+    public class SelectionSort_Tests
+    {
+        //################################################################################
+        #region Fields
+
+        private IntArrayGenerator m_ArrayGenerator;
+
+        #endregion
+
+        //################################################################################
+        #region Tests
+
+        [TestMethod()]
+        public void Sort_UnsortedArray_Test()
+        {
+            //Assign
+            m_ArrayGenerator = new IntArrayGenerator(100);
+            var sortedArray = m_ArrayGenerator.GetSorted();
+            var unsortedArray = m_ArrayGenerator.GetUnsorted();
+
+            //Act
+            var selectionSort = new SelectionSort(unsortedArray);
+            selectionSort.Sort();
+
+            //Assert
+            Assert.AreEqual(sortedArray[0], unsortedArray[0]);
+            Assert.AreEqual(sortedArray[10], unsortedArray[10]);
+            Assert.AreEqual(sortedArray[40], unsortedArray[40]);
+            Assert.AreEqual(sortedArray[90], unsortedArray[90]);
+        }
+
+        [TestMethod()]
+        public void Sort_ReversedArray_Test()
+        {
+            //Assign
+            m_ArrayGenerator = new IntArrayGenerator(100);
+            var sortedArray = m_ArrayGenerator.GetSorted();
+            var reversedArray = m_ArrayGenerator.GetReversed();
+
+            //Act
+            var selectionSort = new SelectionSort(reversedArray);
+            selectionSort.Sort();
+
+            //Assert
+            Assert.AreEqual(sortedArray[0], reversedArray[0]);
+            Assert.AreEqual(sortedArray[10], reversedArray[10]);
+            Assert.AreEqual(sortedArray[40], reversedArray[40]);
+            Assert.AreEqual(sortedArray[90], reversedArray[90]);
+        }
+
+        #endregion
+    }
+}
