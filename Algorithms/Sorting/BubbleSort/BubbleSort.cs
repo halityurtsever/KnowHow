06c365e8bb5894d2591ad83b243eca0e7f7a762e
halityurtsever <halityurtsever@gmail.com>
2017-03-21 15:44:02 +0300

    Readme file is added.

--- Algorithms/Sorting/BubbleSort/BubbleSort.cs
+++ Algorithms/Sorting/BubbleSort/BubbleSort.cs
@@ -1,33 +1,7 @@
-﻿
-using CodeHelpers;
+﻿using CodeHelpers;
 
 namespace Algorithms.Sorting.BubbleSort
 {
-    /** Bubble sort algorithm
-     * Swaps [i] and [i + 1] if [i] smaller then [i + 1] until m_Array ordered
-     * 
-     * Initial m_Array    -> 7 - 3 - 9 - 5 - 2 - 8 - 6 - 1 - 4
-     * 
-     * Pass 1           -> 3 - 7 - 9 - 5 - 2 - 8 - 6 - 1 - 4
-     *                     ^---^
-     *                     swap 7 and 3
-     * 
-     * Pass 2           -> 3 - 7 - 5 - 9 - 2 - 8 - 6 - 1 - 4
-     *                             ^---^
-     *                             swap 9 and 5
-     * 
-     * Pass 3           -> 3 - 7 - 5 - 2 - 9 - 8 - 6 - 1 - 4
-     *                                 ^---^
-     *                                 swap 9 and 2
-     * and goes on...
-     * 
-     * Worst case performance: O(n2)
-     * Best case performance: O(n)
-     * Average performance: O(n2)
-     * Worst case space complexity: O(1) auxiliary
-     * 
-     */
-
     public class BubbleSort
     {
         //################################################################################
---
+++ Algorithms/Sorting/BubbleSort/README.md
@@ -0,0 +1,24 @@
+﻿### Bubble sort algorithm
+
+Swaps [i] and [i + 1] if [i] smaller then [i + 1] until array ordered
+
+Initial Array    -> 7 - 3 - 9 - 5 - 2 - 8 - 6 - 1 - 4
+
+Pass 1           -> 3 - 7 - 9 - 5 - 2 - 8 - 6 - 1 - 4
+                    ^---^
+                    swap 7 and 3
+
+Pass 2           -> 3 - 7 - 5 - 9 - 2 - 8 - 6 - 1 - 4
+                            ^---^
+                            swap 9 and 5
+
+Pass 3           -> 3 - 7 - 5 - 2 - 9 - 8 - 6 - 1 - 4
+                                ^---^
+                                swap 9 and 2
+and goes on...
+
+Worst case performance: O(n2)
+Best case performance: O(n)
+Average performance: O(n2)
+Worst case space complexity: O(1) auxiliary
+
