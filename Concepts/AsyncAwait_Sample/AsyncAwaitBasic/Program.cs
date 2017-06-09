b56751bbb27316318452aaf35e1c773329e165fc
halityurtsever <halityurtsever@gmail.com>
2017-06-09 14:14:58 +0300

    Async-await sample on basic level is added to the solution.

---
+++ Concepts/AsyncAwait_Sample/AsyncAwaitBasic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AsyncAwaitBasic
+{
+    class Program
+    {
+        static void Main()
+        {
+            Sample sample = new Sample();
+
+            DateTime t1 = DateTime.Now;
+            Console.WriteLine("Synchronous is started.");
+            sample.PrintPrimeNumbers();
+            var t1_Delta = DateTime.Now.Subtract(t1);
+            Console.WriteLine($"Synchronous is finished and took {t1_Delta}.");
+
+            DateTime t2 = DateTime.Now;
+            Console.WriteLine("Asynchronous is started.");
+            sample.PrintPrimeNumbersAsync();
+            var t2_Delta = DateTime.Now.Subtract(t2);
+            Console.WriteLine($"Asynchronous is finished and took {t2_Delta}.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Concepts/AsyncAwait_Sample/AsyncAwaitBasic/Sample.cs
@@ -0,0 +1,82 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitBasic
+{
+    internal class Sample
+    {
+        //################################################################################
+        #region Fields
+
+        private const int Min = 100;
+        private const int Count = 1000;
+
+        #endregion
+
+        //################################################################################
+        #region Synchronous Implementation
+
+        internal void PrintPrimeNumbers()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var result = GetPrimeNumbers(i * Min + 1, i * Count);
+                result.ToList().ForEach(x => PrintToFile("sync.txt", x));
+            }
+        }
+
+        private IEnumerable<int> GetPrimeNumbers(int min, int count)
+        {
+            return GetPrimeNumbersInSpecificRange(min, count);
+        }
+
+        #endregion
+
+        //################################################################################
+        #region Asynchronous Implementation
+
+        internal async void PrintPrimeNumbersAsync()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //Process doesn't wait for return value, continue its execution
+                //GetPrimeNumbersAsync() method will continue its work in another thread
+                var result = await GetPrimeNumbersAsync(i * Min + 1, i * Count);
+
+                //After result has been gathered, each item in the list will be printed to the file.
+                result.ToList().ForEach(x => PrintToFile("async.txt", x));
+            }
+        }
+
+        private Task<IEnumerable<int>> GetPrimeNumbersAsync(int min, int count)
+        {
+            return Task.Run(() => GetPrimeNumbersInSpecificRange(min, count));
+        }
+
+        #endregion
+
+        //################################################################################
+        #region Private Implementation
+
+        private IEnumerable<int> GetPrimeNumbersInSpecificRange(int min, int count)
+        {
+            return Enumerable.Range(min, count)
+                             .Where(n => Enumerable.Range(2, (int)Math.Sqrt(n) - 1)
+                                                   .All(i => n % i > 0));
+        }
+
+        private void PrintToFile(string filename, int value)
+        {
+            using (StreamWriter streamWriter = File.AppendText($"C:\\tmp\\{filename}"))
+            {
+                streamWriter.WriteLine(value);
+            }
+        }
+
+        #endregion
+    }
+}
