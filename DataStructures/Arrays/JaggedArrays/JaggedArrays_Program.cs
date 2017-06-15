28f5a196879e0c4ca322fc2832847c4bd8794c0d
halityurtsever <halityurtsever@gmail.com>
2017-06-15 15:08:16 +0300

    Arrays (multi dimensional and jagged) are added into DataStructures

---
+++ DataStructures/Arrays/JaggedArrays/JaggedArrays_Program.cs
@@ -0,0 +1,58 @@
+﻿
+namespace DataStructures.Arrays.JaggedArrays
+{
+    class JaggedArrays_Program
+    {
+        //a.k.a Array of Arrays
+
+        static void Main(string[] args)
+        {
+            #region Two Level Jagged Array
+
+            int[][] jagged2 = new int[][] { };
+
+            //Second level isn't allowed to initialize, because of each array size in the top array may varies.
+            int[][] jagged2WithInitials = new int[5][];
+
+            //As you can see each inner array has initialized with different size
+            int[][] jagged2Initialized = new int[3][]
+                                             {
+                                                 new int[] { 1, 2, 3, 4 },
+                                                 new int[] { 3, 4, 5, 6, 7, 8, 9 },
+                                                 new int[] { 5, 6 }
+                                             };
+
+            #endregion
+
+            #region Three Level Jagged Array
+
+            int[][][] jagged3 = new int[][][] { };
+
+            int[][][] jagged3WithInitials = new int[3][][];
+
+            int[][][] jagged3Initialized = new int[3][][]
+                                               {
+                                                   new int[2][]
+                                                       {
+                                                           new int[] { 1, 2, 3 },
+                                                           new int[] { 1, 2, 3, 4, 5 }
+                                                       },
+                                                   new int[4][]
+                                                       {
+                                                           new int[] { 5, 6, 7, 8 },
+                                                           new int[] { 1, 5 },
+                                                           new int[] { 4, 5, 6 },
+                                                           new int[] { 7, 8, 9, 0 }
+                                                       },
+                                                   new int[3][]
+                                                       {
+                                                           new int[] { 1, 3, 4, 5 },
+                                                           new int[] { 4, 5, 6, 7, 8, 9, 0 },
+                                                           new int[] { 9, 0 }
+                                                       }
+                                               };
+
+            #endregion
+        }
+    }
+}
---
+++ DataStructures/Arrays/MultiDimensionalArrays/MultiDimensionalArrays_Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace DataStructures.Arrays.MultiDimensionalArrays
+{
+    class MultiDimensionalArrays_Program
+    {
+        static void Main(string[] args)
+        {
+            #region Two Dimensional Array
+
+            //Two dimensional array
+            int[,] array2D = new int[,] { };
+
+            int[,] array2DInitialized = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+
+            //Two dimensional array with dimension specified
+            int[,] array2DWithDimensions = new int[3, 3];
+
+            int[,] array2DWithDimensionsInitialized = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            #endregion
+
+            #region Three Dimensional Array
+
+            //Three dimensional array
+            int[,,] array3D = new int[,,] { };
+
+            int[,,] array3DInitialized = new int[,,]
+                                             {
+                                                 {
+                                                     { 1, 2 }, { 3, 4 }
+                                                 },
+                                                 {
+                                                     { 5, 6 }, { 7, 8 }
+                                                 }
+                                             };
+
+            //Three dimensional array with dimension specified
+            int[,,] array3DWithDimensions = new int[2, 3, 4];
+
+            int[,,] array3DWithDimensionsInitialized = new int[2, 3, 4]
+                                                           {
+                                                               {
+                                                                   { 1, 2, 3, 4 }, { 2, 3, 4, 5 }, { 3, 4, 5, 6 }
+                                                               },
+                                                               {
+                                                                   { 4, 5, 6, 7 }, { 5, 6, 7, 8 }, { 6, 7, 8, 9 }
+                                                               }
+                                                           };
+
+            #endregion
+
+            PrintArray2D(array2DWithDimensionsInitialized);
+            PrintArray3D(array3DWithDimensionsInitialized);
+        }
+
+        private static void PrintArray2D(int[,] array2D)
+        {
+            for (int i = 0; i < array2D.GetLength(0); i++)
+            {
+                for (int j = 0; j < array2D.GetLength(1); j++)
+                {
+                    Console.WriteLine($"Array[{i},{j}] = {array2D[i, j]}");
+                }
+            }
+        }
+
+        private static void PrintArray3D(int[,,] array3D)
+        {
+            for (int i = 0; i < array3D.GetLength(0); i++)
+            {
+                for (int j = 0; j < array3D.GetLength(1); j++)
+                {
+                    for (int k = 0; k < array3D.GetLength(2); k++)
+                    {
+                        Console.WriteLine($"Array[{i},{j},{k}] = {array3D[i, j, k]}");
+                    }
+                }
+            }
+        }
+    }
+}
